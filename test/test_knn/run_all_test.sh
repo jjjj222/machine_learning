32084fe22b3b304afaa2d0d863a619f6b01426fc
jjjj222 <jjjj222.tso@gmail.com>
2016-04-05 23:21:51 -0500

    test pca

--- test/test_knn/run_all_test.sh
+++ test/test_knn/run_all_test.sh
@@ -1,7 +1,8 @@
 #!/bin/sh -f
 
 #for c in test_knn_basic test_knn_k5 test_knn_k5_weighted
-for c in test_knn_k3 test_knn_pca test_knn_pca_k5
+#for c in test_knn_k3 test_knn_pca test_knn_pca_k5
+for c in test_knn_pca test_knn_pca_k5
 do
     cd $c
     if [ "$1" = "update" ]; then
