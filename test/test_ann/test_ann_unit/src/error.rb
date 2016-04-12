c2dd1d75bb925940471b53b4a30d4d007effd08f
jjjj222 <jjjj222.tso@gmail.com>
2016-04-12 00:08:42 -0500

    remove .res/.diff

--- test/test_ann/test_ann_unit/src/error.rb
+++
@@ -1,5 +0,0 @@
-class ErrorMsg
-    def self.file_line(file, line, msg)
-        puts "Error: In file \"#{file}\" line #{line}: #{msg} !!"
-    end
-end
--- test/test_knn/run_all_test.sh
+++ test/test_knn/run_all_test.sh
@@ -2,7 +2,8 @@
 
 #for c in test_knn_basic test_knn_k5 test_knn_k5_weighted
 #for c in test_knn_k3 test_knn_pca test_knn_pca_k5
-for c in test_knn_pca test_knn_pca_k5
+#for c in test_knn_pca test_knn_pca_k5
+for c in test_knn_pca test_knn_pca_k5 test_knn_k3 test_knn_pca test_knn_pca_k5 test_knn_basic test_knn_k5 test_knn_k5_weighted
 do
     cd $c
     if [ "$1" = "update" ]; then
